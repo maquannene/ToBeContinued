2460b54032c856872b01666faef3593df1ab7bbd
wuhanness <maquan@kingsoft.com>
2015-10-30 22:54:41 +0800

    加入图片处理的库

---
+++ Pods/Target Support Files/NYXImagesKit/NYXImagesKit-umbrella.h
@@ -0,0 +1,19 @@
+#import <UIKit/UIKit.h>
+
+#import "UIImage+Blurring.h"
+#import "UIImage+Enhancing.h"
+#import "UIImage+Filtering.h"
+#import "UIImage+Masking.h"
+#import "UIImage+Reflection.h"
+#import "UIImage+Resizing.h"
+#import "UIImage+Rotating.h"
+#import "UIImage+Saving.h"
+#import "UIScrollView+Screenshot.h"
+#import "UIView+Screenshot.h"
+#import "NYXProgressiveImageView.h"
+#import "NYXImagesHelper.h"
+#import "NYXImagesKit.h"
+
+FOUNDATION_EXPORT double NYXImagesKitVersionNumber;
+FOUNDATION_EXPORT const unsigned char NYXImagesKitVersionString[];
+
