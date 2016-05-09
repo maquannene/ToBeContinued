4d64d1c36c3dbabc925296a3920b97934abcff9c
maquannene <maquan@wps.cn>
2016-05-09 11:03:31 +0800

    修改 NSObject 的 extension 方法

--- vb/Utils/Extension/NSObject_Extension.swift
+++ vb/Utils/Extension/NSObject_Extension.swift
@@ -10,7 +10,7 @@ public extension NSObject {
     
     //  类方法，类也是对象，这里的self 指的就是 这个类
     public class var RealClassName: String {
-        return NSStringFromClass(self).componentsSeparatedByString(".").last!
+        return String(self)
     }
     
     public class var ClassName: String {
@@ -19,7 +19,7 @@ public extension NSObject {
     
     //  实例方法
     public var RealClassName: String {
-        return NSStringFromClass(self.dynamicType).componentsSeparatedByString(".").last!
+        return String(self.dynamicType)
     }
     
     public var ClassName: String {
@@ -27,3 +27,15 @@ public extension NSObject {
     }
     
 }
+
+//class X {
+//
+//}
+//
+//let x = X()
+//
+//var c: AnyClass = X.self
+//c = x.dynamicType
+//
+//var ct: X.Type = X.self
+//ct = self.dynamicType
